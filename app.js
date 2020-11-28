8d8ec3cf02b4e48140411dae979b70e0bc3ef702
igor-stefan <igorstefan@bol.com.br>
2020-11-28 18:06:33 -0300

    ateração3

--- app.js
+++ app.js
@@ -46,10 +46,11 @@ app.post('/dados', (req, res) => {
    };
    Object.assign(now, leitura);
    for(const k in leitura){
-       console.log("K = ", k)
+       console.log("K = ", k, ' K[0] = ', k[0]);
        db('ppm').insert({
            k: k[0],
        });
+       console.log("K = ", k, ' K[1] = ', k[1]);
        db('ugm3').insert({
         k: k[1],
     });
