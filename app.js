21c9ed493760d6c987a2f6a372b50b7cb1a7252f
igor-stefan <igorstefan@bol.com.br>
2020-11-28 21:42:33 -0300

    alterar db

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const app = express();
 
 const db = knex({
     client: 'pg',
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
+    connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
     }
 });
 
